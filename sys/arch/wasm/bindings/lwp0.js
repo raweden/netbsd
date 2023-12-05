b7cb12c8dcf4e95c2741a6aac4e16f990c6c9e6e
Raweden <raweden@live.se>
2023-12-05 19:57:07 +0100

    Small changes for early outlines for means to get time (both wall and monotomic) changing this implementation requires quite some work.

--- sys/arch/wasm/bindings/lwp0.js
+++ sys/arch/wasm/bindings/lwp0.js
@@ -651,6 +651,22 @@ function wasm_sched_add(td, flags) {
 	workers.push(worker);
 }
 
+function kgettimespec64_clock(ts) {
+
+	const now = Date.now();
+
+	kmem.setBigInt64(ts, BigInt(now / 1000), true);					// seconds
+	kmem.setInt32(ts + 8, ((now % 1000) * 1000 * 1000) | 0, true);	// nanoseconds
+}
+
+function kgettimespec64_monotomic(ts) {
+
+	const now = performance.timeOrigin + performance.now();
+	
+	kmem.setBigInt64(ts, BigInt(now / 1000), true);					// seconds
+	kmem.setInt32(ts + 8, ((now % 1000) * 1000 * 1000) | 0, true);	// nanoseconds
+}
+
 function wasm_clock_gettime(ts) {
 
 	let now = performance.timeOrigin + performance.now();
@@ -1439,7 +1455,8 @@ function bootstrap_kern(kmodule) {
 					ucas32: kern_ucas32,
 					spawn_blkdev_worker: kspawn_blkdev_worker,
 					__lock_debug: __lock_debug,
-					kexec_ualloca: __kexec_ualloca,				// khost_ioctl()
+					gettimespec64_clock: kgettimespec64_clock,
+					gettimespec64_monotomic: kgettimespec64_monotomic,
 					// kern_exec
 					execbuf_alloc: kexecbuf_alloc,
 					execbuf_copy: kexecbuf_copy,
